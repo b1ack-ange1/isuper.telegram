739ca66d9bfbe9cdef8c1d8ac123adedf715233f
Super Wang <wangxuepo@gmail.com>
2015-12-17 16:20:03 -0500

    use futurecallback to handle response

--- src/main/java/org/isuper/telegram/TelegramClient.java
+++ src/main/java/org/isuper/telegram/TelegramClient.java
@@ -5,17 +5,18 @@ package org.isuper.telegram;
 
 import java.io.Closeable;
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
+import org.apache.http.ParseException;
 import org.apache.http.StatusLine;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.concurrent.FutureCallback;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 import org.apache.logging.log4j.LogManager;
@@ -28,8 +29,10 @@ import org.isuper.httpclient.AsyncHttpClient;
  */
 public class TelegramClient implements Closeable {
 	
-	private static final Logger LOG = LogManager.getLogger("telegram-client");
+	private static final Logger LOGGER = LogManager.getLogger("telegram-client");
 	
+	private static final TelegramAsyncResponseHandler DEFAULT_ASYNC_RESPONSE_HANDLER = new TelegramAsyncResponseHandler();
+
 	private final AsyncHttpClient client;
 	
 	/**
@@ -56,34 +59,13 @@ public class TelegramClient implements Closeable {
 	/**
 	 * @param token
 	 * 					The token of telegram API
-	 * @param chatIDs
-	 * 					Multiple chat IDs
-	 * @param msg
-	 * 					The content of message
-	 * @param useMarkdown
-	 * 					Parse the message with markdown or not
-	 * @param disablePreview
-	 * 					Disable web page preview or not
-	 */
-	public void sendMessageToMultipleTelegramChats(String token, Long[] chatIDs, String msg, boolean useMarkdown, boolean disablePreview) {
-		if (chatIDs == null || chatIDs.length < 1) {
-			return;
-		}
-		for (long chatID : chatIDs) {
-			sendMessageToSingleTelegramChat(token, chatID, msg, useMarkdown, disablePreview);
-		}
-	}
-	
-	/**
-	 * @param token
-	 * 					The token of telegram API
 	 * @param chatID
 	 * 					Unique identifier for the target chat or username of the target channel (in the format @channelusername)
 	 * @param msg
 	 * 					Text of the message to be sent
 	 */
-	public void sendMessageToSingleTelegramChat(String token, long chatID, String msg) {
-		this.sendMessageToSingleTelegramChat(token, chatID, msg, false, false);
+	public void sendMessage(String token, String chatID, String msg) {
+		this.sendMessage(token, chatID, msg, false, false);
 	}
 
 	/**
@@ -98,7 +80,7 @@ public class TelegramClient implements Closeable {
 	 * @param disablePreview
 	 * 					Disables link previews for links in this message or not
 	 */
-	public void sendMessageToSingleTelegramChat(String token, long chatID, String msg, boolean useMarkdown, boolean disablePreview) {
+	public void sendMessage(String token, String chatID, String msg, boolean useMarkdown, boolean disablePreview) {
 		List<NameValuePair> items = new LinkedList<>();
 		items.add(new BasicNameValuePair("chat_id", "" + chatID));
 		items.add(new BasicNameValuePair("text", "" + msg));
@@ -106,7 +88,7 @@ public class TelegramClient implements Closeable {
 			items.add(new BasicNameValuePair("parse_mode", "Markdown"));
 		}
 		items.add(new BasicNameValuePair("disable_web_page_preview", "" + disablePreview));
-		this.sendMessage(token, items);
+		this.send(token, items);
 	}
 
 	/**
@@ -119,22 +101,8 @@ public class TelegramClient implements Closeable {
 	 * @param msg
 	 * 					Text of the message to be sent
 	 */
-	public void sendReplyMessage(String token, Long chatID, long replyTo, String msg) {
-		this.sendReplyMessage(token, chatID.toString(), replyTo, msg, false, false);
-	}
-
-	/**
-	 * @param token
-	 * 					The token of telegram API
-	 * @param chatID
-	 * 					Unique identifier for the target chat or username of the target channel (in the format @channelusername)
-	 * @param replyTo
-	 * 					If the message is a reply, ID of the original message
-	 * @param msg
-	 * 					Text of the message to be sent
-	 */
-	public void sendReplyMessage(String token, String chatID, long replyTo, String msg) {
-		this.sendReplyMessage(token, chatID, replyTo, msg, false, false);
+	public void replyMessage(String token, String chatID, long replyTo, String msg) {
+		this.replyMessage(token, chatID, replyTo, msg, false, false);
 	}
 
 	/**
@@ -151,7 +119,7 @@ public class TelegramClient implements Closeable {
 	 * @param disablePreview
 	 * 					Disables link previews for links in this message or not
 	 */
-	public void sendReplyMessage(String token, String chatID, long replyTo, String msg, boolean useMarkdown, boolean disablePreview) {
+	public void replyMessage(String token, String chatID, long replyTo, String msg, boolean useMarkdown, boolean disablePreview) {
 		List<NameValuePair> items = new LinkedList<>();
 		items.add(new BasicNameValuePair("chat_id", chatID));
 		items.add(new BasicNameValuePair("text", "" + msg));
@@ -160,34 +128,14 @@ public class TelegramClient implements Closeable {
 		}
 		items.add(new BasicNameValuePair("disable_web_page_preview", "" + disablePreview));
 		items.add(new BasicNameValuePair("reply_to_message_id", "" + replyTo));
-		this.sendMessage(token, items);
+		this.send(token, items);
 	}
 
-	private void sendMessage(String token, List<NameValuePair> items) {
-		try {
-			HttpPost post = new HttpPost("https://api.telegram.org/bot" + token + "/sendMessage");
-			post.setEntity(new UrlEncodedFormEntity(items, "UTF-8"));
-			
-			LOG.trace(post.getRequestLine());
-
-			Future<HttpResponse> future = this.client.execute(post, null);
-			HttpResponse resp = future.get();
-			
-			StatusLine status = resp.getStatusLine();
-			HttpEntity entity = resp.getEntity();
-			String contentType = entity.getContentType().getValue();
-			String content = EntityUtils.toString(entity);
-			
-			LOG.trace(resp.getStatusLine());
-			LOG.trace(contentType);
-			LOG.trace(content);
-			
-			if (status.getStatusCode() != 200) {
-				throw new IOException(String.format("%d response received from server: %s", status.getStatusCode(), content));
-			}
-		} catch (InterruptedException | ExecutionException | IllegalStateException | IOException e) {
-			LOG.error(e.getLocalizedMessage(), e);
-		}
+	private void send(String token, List<NameValuePair> items) {
+		HttpPost post = new HttpPost("https://api.telegram.org/bot" + token + "/sendMessage");
+		post.setEntity(new UrlEncodedFormEntity(items, Charset.forName("UTF-8")));
+		LOGGER.trace(post.getRequestLine());
+		this.client.execute(post, DEFAULT_ASYNC_RESPONSE_HANDLER);
 	}
 
 	/* (non-Javadoc)
@@ -196,6 +144,49 @@ public class TelegramClient implements Closeable {
 	@Override
 	public void close() throws IOException {
 		this.client.close();
+	}
+	
+	private static class TelegramAsyncResponseHandler implements FutureCallback<HttpResponse> {
+
+		/* (non-Javadoc)
+		 * @see org.apache.http.concurrent.FutureCallback#completed(java.lang.Object)
+		 */
+		@Override
+		public void completed(HttpResponse resp) {
+			StatusLine status = resp.getStatusLine();
+			LOGGER.trace(resp.getStatusLine());
+			
+			HttpEntity entity = resp.getEntity();
+			String contentType = entity.getContentType().getValue();
+			LOGGER.trace(contentType);
+			
+			try {
+				String content = EntityUtils.toString(entity);
+				LOGGER.trace(content);
+				if (status.getStatusCode() != 200) {
+					LOGGER.warn(String.format("%d response received from server: %s", status.getStatusCode(), content));
+				}
+			} catch (ParseException | IOException e) {
+				LOGGER.error(e.getMessage(), e);
+			}
+		}
+
+		/* (non-Javadoc)
+		 * @see org.apache.http.concurrent.FutureCallback#failed(java.lang.Exception)
+		 */
+		@Override
+		public void failed(Exception ex) {
+			LOGGER.error(ex.getMessage(), ex);
+		}
+
+		/* (non-Javadoc)
+		 * @see org.apache.http.concurrent.FutureCallback#cancelled()
+		 */
+		@Override
+		public void cancelled() {
+			LOGGER.error("Request to telegram server has been canncelled.");
+		}
+		
 	}
 
 }
